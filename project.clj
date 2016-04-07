a28caef58f94cb531c5e9017afce2c1b04ba5639
Chris Ford <christophertford@gmail.com>
2016-04-07 20:59:02 +0100

    First release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-bach "0.1.0-SNAPSHOT"
+(defproject cljs-bach "0.1.0"
   :description "A Clojurescript wrapper for the Web Audio API."
   :license {:name "MIT" }
   :dependencies [[org.clojure/clojure "1.7.0"]
